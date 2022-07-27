d0b1544b33a48d4dc8aa823bd0f0859e3279339e
melashu <meshu102@outlook.com>
2022-07-27 14:46:03 +0300

    Add event listner for each nav list

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,23 +1,31 @@
-const menuIcon = document.getElementById('menu-icon');
-const main = document.querySelector('main');
-const header = document.querySelector('header');
-const btnClose = document.getElementById('btn-close');
-const navBar = document.querySelector('nav');
-const body = document.querySelector('body');
+const menuIcon = document.getElementById("menu-icon");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const btnClose = document.getElementById("btn-close");
+const navBar = document.querySelector("nav");
+const body = document.querySelector("body");
 
-menuIcon.addEventListener('click', () => {
-  main.style.display = 'none';
-  body.style.overflowY = 'hidden';
+menuIcon.addEventListener("click", () => {
+  main.style.display = "none";
+  body.style.overflowY = "hidden";
 
-  navBar.style.display = 'flex';
-  header.style.height = '100vh';
+  navBar.style.display = "flex";
+  header.style.height = "100vh";
   header.style.backgroundImage = "url('../image/mob-bac.png')";
 });
 
-btnClose.addEventListener('click', () => {
-  main.style.display = 'block';
-  body.style.overflowY = 'visible';
+function displayMain() {
+  main.style.display = "block";
+  body.style.overflowY = "visible";
 
-  navBar.style.display = 'none';
-  header.style.height = '0';
+  navBar.style.display = "none";
+  header.style.height = "0";
+  window.location.reload();
+}
+
+btnClose.addEventListener("click", displayMain);
+
+const menuList = document.querySelectorAll(".nav-menu li");
+menuList.forEach((list) => {
+  list.addEventListener("click", displayMain);
 });
