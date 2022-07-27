af1097c4c6395041516177f490b6c39e2d90c0bb
melashu <meshu102@outlook.com>
2022-07-27 07:52:35 +0300

    Add js script for product.js

---
+++ js/product.js
@@ -0,0 +1,50 @@
+const mainProgram = document.getElementById("main-program");
+const productList = [
+  {
+    img: "../image/telebirrLogo.png",
+    imgAlt: "Telbirr",
+    title: "TelBirr",
+    catagory: "Finance and Mobile Money Transfer",
+    description:
+      "Telbirr is a mobile money platform it can handle as a mobile walet. Using telbirr you can transfer money from different bank",
+  },
+  {
+    img: "../image/alewaLogo.png",
+    imgAlt: "Alewa Stock",
+    title: "Alewa Stock",
+    catagory: "Business Management",
+    description:
+      "Alewa Stock was design in 2021, it is a bussiness management platform",
+  },
+  {
+    img: "../image/sekelaLogo.png",
+    imgAlt: "Sekela",
+    title: "Sekela Commerece",
+    catagory: "E-commerece",
+    description:
+      "Sekela Commerece is a B2B and B2C platform, which connect buyers and sellers",
+  },
+  {
+    img: "../image/myethio.png",
+    imgAlt: "My-Ethiotel",
+    title: "My-Ethiotel",
+    catagory: "Telcom",
+    description: "My-Ethiotel is designed in 2018, It is used to recharge mobile air time",
+  },
+  {
+    img: "../image/chat.png",
+    imgAlt: "Let Chat",
+    title: "Let Chat",
+    catagory: "Social Media",
+    description:
+      "Let Chat is a social media platform, which can interconnect people arround the glob",
+  },
+  {
+    img: "../image/fidle.png",
+    imgAlt: "Fidle",
+    title: "Fidle",
+    catagory: "Education",
+    description:
+      "Fidle educational platform which is designed to teach kids about Amaharic language",
+  },
+];
